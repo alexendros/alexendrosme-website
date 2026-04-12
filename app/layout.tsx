bf6db64ea74d2628d085d1522145abd61b2ba308
Alexendros <developer@alexendros.me>
2026-04-12 23:36:12 +0200

    Revert "Merge pull request #5 from alexendros/vercel/install-vercel-web-analytics-t3l7xu"
    This reverts commit d758f5bc1a490948355de1951a9ae16bf4f4d372, reversing
    changes made to 784790988a27ac9bddf485bbc6bbe1dd0afa57a3.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next";
 import { Nav } from "@/components/nav";
 import { Footer } from "@/components/footer";
 import { siteConfig } from "@/lib/site";
@@ -72,7 +71,6 @@ export default function RootLayout({
         <Nav />
         <main className="flex-1">{children}</main>
         <Footer />
-        <Analytics />
       </body>
     </html>
   );
